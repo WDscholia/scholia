64cb8a6ab5db3899eb010d99f772ff8d7a39dbf3
fnielsen <fn@imm.dtu.dk>
2016-10-10 14:35:18 +0200

    Create __main__.py

---
+++ scholia/__main__.py
@@ -0,0 +1,23 @@
+"""query
+
+Usage:
+  scholia orcid-to-q <orcid>
+
+Examples:
+  $ python -m scholia orcid-to-q 0000-0001-6128-3356
+  Q20980928
+
+"""
+
+
+from docopt import docopt
+
+from .query import orcid_to_qs
+
+
+arguments = docopt(__doc__)
+    
+if arguments['orcid-to-q']:
+    qs = orcid_to_qs(arguments['<orcid>'])
+    if len(qs) > 0:
+        print(qs[0])
