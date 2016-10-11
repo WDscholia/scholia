0f3e6a129a110f66bf6df5994e590689c502da1d
Finn Aarup Nielsen <faan@dtu.dk>
2016-10-11 23:34:55 +0200

    Add bootstrap

--- scholia/app/__init__.py
+++ scholia/app/__init__.py
@@ -2,8 +2,10 @@
 from __future__ import absolute_import, division, print_function
 
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 from . import views
 
