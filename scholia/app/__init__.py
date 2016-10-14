bce988bd4eaf447385cc4dd8dca16e9c0207d7e3
Finn Aarup Nielsen <faan@dtu.dk>
2016-10-14 08:35:12 +0200

    Serve local assets

--- scholia/app/__init__.py
+++ scholia/app/__init__.py
@@ -4,9 +4,16 @@
 from __future__ import absolute_import, division, print_function
 
 from flask import Flask
-from flask_bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap, StaticCDN
+
 
 app = Flask(__name__)
 Bootstrap(app)
+
+# Serve assets from wmflabs for privacy reasons
+app.extensions['bootstrap']['cdns']['jquery'] = StaticCDN()
+app.extensions['bootstrap']['cdns']['bootstrap'] = StaticCDN(
+    static_endpoint='serve_bootstrap_custom')
+
 
 from . import views
