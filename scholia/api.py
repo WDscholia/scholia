97127b12b8b58fe297c5ea759e46b305cf575456
fnielsen <fn@imm.dtu.dk>
2016-12-20 22:49:20 +0100

    Implement more extractors

--- scholia/api.py
+++ scholia/api.py
@@ -15,6 +15,12 @@ from __future__ import print_function
 import requests
 
 
+MONTH_NUMBER_TO_MONTH = {
+    'en': ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+           'August', 'September', 'October', 'November', 'December']
+}
+
+
 def wb_get_entities(qs):
     """Get entities from Wikidata.
 
@@ -62,14 +68,38 @@ def entity_to_authors(entity):
         Title as string.
 
     """
+    def statement_to_order(statement):
+        LARGE = 10000000
+        if 'P1545' in statement['qualifiers']:
+            for qualifier in statement['qualifiers']['P1545']:
+                # The data value is a string type
+                # TODO: handle exceptions?
+                return int(qualifier['datavalue']['value'])
+        else:
+            return LARGE
+
     qs = []
+    orders = []
     claims = entity['claims']
     if 'P50' in claims:
         for statement in claims['P50']:
             qs.append(statement['mainsnak']['datavalue']['value']['id'])
+            orders.append(statement_to_order(statement))
     entities = wb_get_entities(qs)
-    authors = [entity_to_label(entities[q]) for q in qs]
-    return authors
+
+    authornames = []
+    if 'P2093' in claims:
+        for statement in claims['P2093']:
+            value = statement['mainsnak']['datavalue']['value']
+            authornames.append((statement_to_order(statement), value))
+
+    authors = [(order, entity_to_label(entities[q]))
+               for order, q in zip(orders, qs)]
+    authors.extend(authornames)
+
+    authors.sort()
+
+    return [author for _, author in authors]
 
 
 def entity_to_label(entity):
@@ -92,13 +122,40 @@ def entity_to_label(entity):
     return ''
 
 
+def entity_to_month(entity, language='en'):
+    """Extract month of publication from entity.
+
+    Parameters
+    ----------
+    entity : dict
+        Dictionary with Wikidata item.
+    language : str
+        Language, if none, returns the month as a string with the month number.
+
+    Returns
+    -------
+    month : str
+        Month as string.
+
+    """
+    for statement in entity['claims']['P577']:
+        date = statement['mainsnak']['datavalue']['value']['time']
+        month = date[6:8]
+        if language is None:
+            return month
+        elif language == 'en':
+            return MONTH_NUMBER_TO_MONTH['en'][int(month)]
+        else:
+            raise ValueError('language "{}" not support'.format(language))
+
+
 def entity_to_title(entity):
     """Extract title from entity.
 
     Parameters
     ----------
     entity : dict
-        Dictionary with Wikidata item
+        Dictionary with Wikidata item.
 
     Returns
     -------
@@ -109,6 +166,26 @@ def entity_to_title(entity):
     for statement in entity['claims']['P1476']:
         title = statement['mainsnak']['datavalue']['value']['text']
         return title
+
+
+def entity_to_year(entity):
+    """Extract year of publication from entity.
+
+    Parameters
+    ----------
+    entity : dict
+        Dictionary with Wikidata item
+
+    Returns
+    -------
+    year : str
+        Year as string.
+
+    """
+    for statement in entity['claims']['P577']:
+        date = statement['mainsnak']['datavalue']['value']['time']
+        year = date[1:5]
+        return year
 
 
 def main():
