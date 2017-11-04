faf1ea0bfd3f35598e2f68442e80fc830a3173a6
Egon Willighagen <egon.willighagen@gmail.com>
2017-11-04 16:23:21 +0100

    Template for taxons

--- scholia/query.py
+++ scholia/query.py
@@ -332,6 +332,10 @@ def q_to_class(q):
             ]):
         class_ = 'pathway'
     elif set(classes).intersection([
+            'XXX',  # biological pathway
+            ]):
+        class_ = 'taxon'
+    elif set(classes).intersection([
             'Q7397',  # software
             'Q1639024',  # mathematical software
             'Q21127166',  # Java software library
