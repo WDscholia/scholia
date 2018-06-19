a9ad2d9b3bed2fb982b97d8794afb92a2afe4c97
Finn Aarup Nielsen <faan@dtu.dk>
2018-06-19 11:26:30 +0200

    Change the default app creation The app take a long time to start up due to the set up of the text mining. This change disable the text mining and enable the third parties application, which currently is only the chemical visualization

--- runserver.py
+++ runserver.py
@@ -2,8 +2,8 @@ from scholia.app import create_app
 
 
 app = create_app(
-    text_to_topic_q_text_enabled=True,
-    third_parties_enabled=False)
+    text_to_topic_q_text_enabled=False,
+    third_parties_enabled=True)
 app.config['APPLICATION_ROOT'] = '/'
 
 if __name__ == '__main__':
