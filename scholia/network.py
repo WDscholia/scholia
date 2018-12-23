1ba88f930bee07a34a40ae940b5309450d25caad
Egon Willighagen <egon.willighagen@gmail.com>
2018-12-23 16:39:41 +0100

    Use AUTO_LANGUAGE everywhere

--- scholia/network.py
+++ scholia/network.py
@@ -23,8 +23,7 @@ WITH {
 } AS %result
 WHERE {
   INCLUDE %result
-  SERVICE wikibase:label {
-    bd:serviceParam wikibase:language "en,fr,de,ru,es,zh,jp". }
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
 }
 """
 
