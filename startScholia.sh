8286d155895874257b65c4e67f6884ea0f8f3431
nunogit <nunosn@iol.pt>
2020-02-10 15:48:10 +0100

    bash start script removed

--- startScholia.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#export HTTP_PROXY="http://proxy-scholiaproxy7.prod.openrisknet.org/"
-#export HTTPS_PROXY="http://proxy-scholiaproxy7.prod.openrisknet.org/"
-#export HTTP_PROXY="http://127.0.0.1:8080/"
-#export HTTPS_PROXY="http://127.0.0.1:8080/"
-
-python3 runserver.py
