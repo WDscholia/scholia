20243a6ae484e985d8c6d80798a004a4f13db000
nunogit <nunosn@iol.pt>
2020-02-25 16:22:34 +0100

    adding an entrypoint to run with a wsgi server - issue #1058

---
+++ wsgi.py
@@ -0,0 +1,10 @@
+from scholia.app import create_app
+from scholia.config import config
+
+app = create_app(
+    text_to_topic_q_text_enabled=False,
+    third_parties_enabled=True)
+app.config['APPLICATION_ROOT'] = '/'
+
+if __name__ == "__main__":
+    app.run()
