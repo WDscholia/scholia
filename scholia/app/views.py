0591e3720104e6c7c388dfe63880b112b8f9489f
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-01-24 03:38:01 +0100

    Fix name and more abstracting

--- scholia/app/views.py
+++ scholia/app/views.py
@@ -142,7 +142,7 @@ def show_biorxiv(biorxiv_id):
         Rendered HTML.
     """
     qs = biorxiv_to_qs(biorxiv_id)
-    return _render_qs(qs)
+    return _render_work_qs(qs)
 
 
 @main.route('/arxiv/<arxiv>')
@@ -165,10 +165,10 @@ def show_arxiv(arxiv):
 
     """
     qs = arxiv_to_qs(arxiv)
-    return _render_qs(qs)
+    return _render_work_qs(qs)
 
 
-def _render_qs(qs):
+def _render_work_qs(qs):
     if len(qs) > 0:
         q = qs[0]
         return redirect(url_for('app.show_work', q=q), code=302)
@@ -938,10 +938,7 @@ def redirect_pubmed(pmid):
 
     """
     qs = pubmed_to_qs(pmid)
-    if len(qs) > 0:
-        q = qs[0]
-        return redirect(url_for('app.show_work', q=q), code=302)
-    return render_template('404.html')
+    return _render_work_qs(qs)
 
 
 @main.route('/ncbi-taxon/<taxon>')
