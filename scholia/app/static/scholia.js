f0005c5a2563ad9c43410a5218ee6449fe0c9f24
Cesar Uribe <uribeleon@gmail.com>
2021-07-21 23:42:00 -0500

    extract some js codes outside base.html

--- scholia/app/static/scholia.js
+++ scholia/app/static/scholia.js
@@ -494,3 +494,395 @@ function sparqlToShortInchiKey(sparql, key,  element, filename) {
     new_sparql = sparql.replace("_shortkey_",shortkey)
     sparqlToDataTable(new_sparql, element, filename);
 }
+};
+
+
+function workWithQ(q) {
+    var url = 'https://www.wikidata.org/w/api.php?action=wbgetentities&ids=' +
+	        q  + 
+	       '&format=json&callback=?';
+     
+    $.getJSON(url, function (data) {
+	 var item = data.entities[q];
+	 if ('en' in item.labels) {
+	     $("#h1").text(item.labels.en.value);
+	 }
+	 $("#h1").append(' (<a href="https://www.wikidata.org/wiki/' +  q  + ' ">' + q  + '</a>)');
+	 
+	 var detailsList = Array();
+	 try {
+	     var orcid = item.claims.P496[0].mainsnak.datavalue.value;
+	     detailsList.push( '<a href="https://orcid.org/"><img alt="' +
+			       'ORCID logo" src="{{ url_for(\'static\', filename=\'images/orcid_16x16.gif\') }}"' +
+			       'width="16" height="16" hspace="4" /></a> <a href="https://orcid.org/' +
+			       encodeURI( orcid ) + '">https://orcid.org/' + escapeHTML( orcid ) + '</a>');
+	 }
+	 catch(e) {}
+
+	 try {s
+	     var mastodonAccount = item.claims.P4033[0].mainsnak.datavalue.value;
+	     var mastodonComponents = mastodonAccount.split("@");
+	     if (mastodonComponents.length == 2) {
+		 detailsList.push( '<img alt="Twitter t icon" src="' +
+				   'https://upload.wikimedia.org/wikipedia/commons/4/48/Mastodon_Logotype_%28Simple%29.svg' +
+				   '" width="16" height="16" hspace="4" /> <a href="https://' + mastodonComponents[1] + '/@' +
+				   encodeURI( mastodonComponents[0] ) + '">@' + escapeHTML( mastodonComponents[0] ) + '</a> ');
+	     }
+	 }
+	 catch(e) { }
+
+	 try {
+	     var twitterAccount = item.claims.P2002[0].mainsnak.datavalue.value;
+	     detailsList.push( '<img alt="Twitter t icon" src="' +
+			       'https://upload.wikimedia.org/wikipedia/commons/8/8b/Twitter_logo_initial.svg' +
+			       '" width="16" height="16" hspace="4" /> <a href="https://twitter.com/' +
+			       encodeURI( twitterAccount ) + '">@' + escapeHTML( twitterAccount ) + '</a> ');
+	 }
+	 catch(e) { }
+
+	 try {
+	     var rorID = item.claims.P6782[0].mainsnak.datavalue.value;
+	     detailsList.push( 'ROR <a href="https://ror.org/' + encodeURI( rorID ) + '">' + rorID + '</a> ');
+	 }
+	 catch(e) { }
+
+	 try {
+	     var hashtag = item.claims.P2572[0].mainsnak.datavalue.value;
+	     detailsList.push( '<a href="https://hashtags-hub.toolforge.org/' +
+			       encodeURI( hashtag ) + '">#' + escapeHTML( hashtag ) + '</a> ');
+	 }
+	 catch(e) {}
+
+	 try {
+	     var doi = item.claims.P356[0].mainsnak.datavalue.value;
+	     $("head").append( '<meta name="citation_doi" content="' + doi + '"/>' );
+	 }
+	 catch(e) {}
+
+	 try {
+	     $( '#details' ).append( detailsList.join( " | " ) );
+	 }
+	 catch(e) {}
+	 
+	 /* BioSchemas annotation */
+	 if (item.claims.P31 &&
+	     ((item.claims.P31[0].mainsnak.datavalue.value.id == 'Q5'))) {
+	   try { /* Person */
+	       bioschemasAnnotation = {
+	          "@context" : "https://schema.org",
+	          "@type" : "Person" ,
+	          "http://purl.org/dc/terms/conformsTo": { "@type": "CreativeWork", "@id": "https://bioschemas.org/profiles/Person/0.2-DRAFT-2019_07_19/" },
+	          "description" : "A person" ,
+	          "identifier" : "{{ q }}" ,
+	          "mainEntityOfPage" : "http://www.wikidata.org/entity/{{ q }}"
+	       }
+	       if ('en' in item.labels) {
+	         bioschemasAnnotation.name = item.labels.en.value;
+	       }
+	       $( '#bioschemas' ).append( JSON.stringify(bioschemasAnnotation) );
+	    } catch(e) {}
+	 } else if (item.claims.P31 &&
+	     ((item.claims.P31[0].mainsnak.datavalue.value.id == 'Q47461491') ||
+	      (item.claims.P31[0].mainsnak.datavalue.value.id == 'Q967847'))) {
+	   try { /* ChemicalSubstance */
+	       bioschemasAnnotation = {
+	          "@context" : "https://schema.org",
+	          "@type" : "ChemicalSubstance" ,
+	          "http://purl.org/dc/terms/conformsTo": { "@type": "CreativeWork", "@id": "https://bioschemas.org/profiles/ChemicalSubstance/0.4-RELEASE/" },
+	          "identifier" : "{{ q }}" ,
+	          "url" : "http://www.wikidata.org/entity/{{ q }}"
+	       }
+	       if ('en' in item.labels) {
+	         bioschemasAnnotation.name = item.labels.en.value;
+	       }
+	       $( '#bioschemas' ).append( JSON.stringify(bioschemasAnnotation) );
+	    } catch(e) {}
+	 } else if (item.claims.P225) {
+             try { /* Taxon */
+		 var taxonName = item.claims.P225[0].mainsnak.datavalue.value;
+		 bioschemasAnnotation = {
+	             "@context" : "https://schema.org",
+	             "@type" : "Taxon" ,
+	             "http://purl.org/dc/terms/conformsTo": { "@type": "CreativeWork", "@id": "https://bioschemas.org/profiles/Taxon/0.6-RELEASE/" },
+	             "name" : taxonName ,
+	             "url" : "http://www.wikidata.org/entity/{{ q }}"
+		 }
+		 if (item.claims.P105) {
+	             var taxonRank = item.claims.P105[0].mainsnak.datavalue.value.id;
+	             bioschemasAnnotation.taxonRank = "http://www.wikidata.org/entity/" + taxonRank ;
+		 }
+		 if (item.claims.P171) {
+	             var parent = item.claims.P171[0].mainsnak.datavalue.value.id;
+	             bioschemasAnnotation.parentTaxon = "http://www.wikidata.org/entity/" + parent ;
+		 }
+		 $( '#bioschemas' ).append( JSON.stringify(bioschemasAnnotation) );
+		 // console.log(JSON.stringify(bioschemasAnnotation, "", 2))
+	     } catch(e) {}
+	 } else if (item.claims.P235) {
+	     try { /* Chemical Compound */
+		 var inchiKey = item.claims.P235[0].mainsnak.datavalue.value;
+		 bioschemasAnnotation = {
+	             "@context" : "https://schema.org",
+	             "@type" : "MolecularEntity" ,
+	             "http://purl.org/dc/terms/conformsTo": { "@type": "CreativeWork", "@id": "https://bioschemas.org/profiles/MolecularEntity/0.5-RELEASE/" },
+	             "identifier" : "{{ q }}" ,
+	             "inChIKey" : inchiKey ,
+	             "url" : "http://www.wikidata.org/entity/{{ q }}"
+		 }
+		 if ('en' in item.labels) {
+	         bioschemasAnnotation.name = item.labels.en.value;
+         }
+		 if (item.claims.P234 && item.claims.P234[0].mainsnak.datavalue) {
+	             var inchi = item.claims.P234[0].mainsnak.datavalue.value;
+	             bioschemasAnnotation.inChI = inchi ;
+		 }
+		 if (item.claims.P274 && item.claims.P274[0].mainsnak.datavalue) {
+	             var chemformula = item.claims.P274[0].mainsnak.datavalue.value;
+	             bioschemasAnnotation.molecularFormula = chemformula ;
+		 }
+		 if (item.claims.P2017 && item.claims.P2017[0].mainsnak.datavalue) {
+	             var smiles = item.claims.P2017[0].mainsnak.datavalue.value;
+	             bioschemasAnnotation.molecularFormula = smiles.replace("\"", "\'\'") ;
+		 } else if (item.claims.P233 && item.claims.P233[0].mainsnak.datavalue) {
+	             var smiles = item.claims.P233[0].mainsnak.datavalue.value;
+	             bioschemasAnnotation.smiles = smiles.replace("\"", "\'\'") ;
+		 }
+		 $( '#bioschemas' ).append( JSON.stringify(bioschemasAnnotation) );
+	     } catch(e) { console.log("Exception: " + e)
+	     }
+	 } else if (item.claims.P352) { // UniProt ID
+	     try { /* Protein */
+		 var uniprot = item.claims.P352[0].mainsnak.datavalue.value;
+		 bioschemasAnnotation = {
+	             "@context" : "https://schema.org",
+	             "@type" : "Protein" ,
+	             "http://purl.org/dc/terms/conformsTo": { "@type": "CreativeWork", "@id": "https://bioschemas.org/profiles/Protein/0.11-RELEASE/" },
+	             "identifier" : "{{ q }}" ,
+	             "url" : "http://www.wikidata.org/entity/{{ q }}" ,
+	             "sameAs": "https://www.uniprot.org/uniprot/" + uniprot
+		 }
+		 if ('en' in item.labels) {
+	       bioschemasAnnotation.name = item.labels.en.value;
+         }
+		 $( '#bioschemas' ).append( JSON.stringify(bioschemasAnnotation) );
+	     } catch(e) { console.log("Exception: " + e)
+	     }
+	 } else if (item.claims.P351 || item.claims.P594) { // NCBI Gene or Ensembl
+	     try { /* Gene */
+		 bioschemasAnnotation = {
+	             "@context" : "https://schema.org",
+	             "@type" : "Gene" ,
+	             "http://purl.org/dc/terms/conformsTo": { "@type": "CreativeWork", "@id": "https://bioschemas.org/profiles/Gene/0.7-RELEASE/" },
+	             "identifier" : "{{ q }}" ,
+	             "url" : "http://www.wikidata.org/entity/{{ q }}"
+		 }
+		 if ('en' in item.labels) {
+	       bioschemasAnnotation.name = item.labels.en.value;
+         }
+		 counter = 0
+		 bioschemasAnnotation.sameAs = []
+		 if (item.claims.P351 && item.claims.P351[0].mainsnak.datavalue) {
+	             var ncbi = item.claims.P351[0].mainsnak.datavalue.value;
+	             bioschemasAnnotation.sameAs[counter] = "https://www.ncbi.nlm.nih.gov/gene/" + ncbi;
+	             counter++
+		 }
+		 if (item.claims.P594 && item.claims.P594[0].mainsnak.datavalue) {
+	             var ensembl = item.claims.P594[0].mainsnak.datavalue.value;
+	             bioschemasAnnotation.sameAs[counter] = "http://identifiers.org/ensembl/" + ensembl;
+		 }
+		 $( '#bioschemas' ).append( JSON.stringify(bioschemasAnnotation) );
+	     } catch(e) { console.log("Exception: " + e)
+	     }
+	 }
+	 
+	 /* English Wikipedia */
+	 if ('enwiki' in item.sitelinks) {
+	     var title = item.sitelinks.enwiki.title;
+	     var wikipediaApiUrl = 'https://en.wikipedia.org/w/api.php?' +
+				   'action=query&prop=extracts&exsentences=3&exlimit=1&exintro=1&' + 
+				   'explaintext=1&callback=?&format=json&titles=' +
+				   encodeURIComponent(title);
+	     var wikipediaUrl = 'https://en.wikipedia.org/wiki/' + encodeURIComponent(title)
+	     
+	     $.getJSON(wikipediaApiUrl, function(data) {
+		 var pages = data.query.pages;
+		 var text = pages[Object.keys(pages)[0]].extract + " ... "
+		 var html = "(from the <a href=\"" + wikipediaUrl + "\">English Wikipedia</a>)";
+		 $("#intro").text(text).append(html);
+	     }).fail(function(d, textStatus, error) {
+		 console.error("getJSON failed, status: " + textStatus + ", error: "+error)
+	     });
+	 }
+
+	 
+	 /* English Wikiversity */
+	 if ('enwikiversity' in item.sitelinks) {
+	     var enwikiversityTitle = item.sitelinks.enwikiversity.title;
+	     var wikiversityApiUrl = 'https://en.wikiversity.org/w/api.php?' +
+				     'action=query&prop=extracts&exsentences=3&exlimit=1&exintro=1&' + 
+				     'explaintext=1&callback=?&format=json&titles=' +
+				     encodeURIComponent(enwikiversityTitle);
+	     var wikiversityUrl = 'https://en.wikiversity.org/wiki/' + encodeURIComponent(enwikiversityTitle)
+
+	     $.getJSON(wikiversityApiUrl, function(data) {
+		 var pages = data.query.pages;
+		 var text = pages[Object.keys(pages)[0]].extract; 
+		 if (text) {
+		     var html = "... (from the <a href=\"" + wikiversityUrl + "\">English Wikiversity</a>)";
+		 }
+		 else {
+		     var html = "Read on the <a href=\"" + wikiversityUrl + "\">English Wikiversity</a>";
+		 }
+		 $("#wikiversity-extract").text(text).append(html);
+	     }).fail(function(d, textStatus, error) {
+		 console.error("getJSON failed, status: " + textStatus + ", error: "+error)
+	     });
+	 }
+
+     });
+}
+ 
+function workWithQ2(q2) {
+    var url2 = 'https://www.wikidata.org/w/api.php?action=wbgetentities&ids=' +
+				   q2  + 
+				   '&format=json&callback=?';
+		$.getJSON(url2, function (data) {
+			 var item = data.entities[q2];
+			 if ('en' in item.labels) {
+				 $("#h1").append(' - '+item.labels.en.value);
+			 }
+			 $("#h1").append(' (<a href="https://www.wikidata.org/wiki/'+ q2 + '">' +  q2 + '</a>)');	
+		});
+    
+}
+
+function wikiItemAsDifferentAspect(query, q, urlfor) {
+    
+    var endpointUrl = 'https://query.wikidata.org/sparql';
+    settings = {
+    headers: { Accept: 'application/sparql-results+json' },
+    data: { query: query }
+    };
+    
+    $.ajax( endpointUrl, settings ).then( function ( data ) {
+    data.results.bindings.forEach(function(entry) {
+        var aspect = entry.aspect.value;
+        var href = urlfor + aspect + '/' + q 
+        $( '#aspect-chooser' ).append(
+        `<a role="button" class="btn btn-outline-dark btn-sm" href="${href} "> ${aspect} </a> ` )
+    });
+    } );
+}
+
+function wikiItemSubpage(query, q, urlfor) {
+    var endpointUrl = 'https://query.wikidata.org/sparql';
+    settings = {
+    headers: { Accept: 'application/sparql-results+json' },
+    data: { query: query }
+    };
+
+    $.ajax( endpointUrl, settings ).then( function ( data ) {
+    data.results.bindings.forEach(function(entry) {
+        var props = entry.aspectsubpage.value.split("-");
+        var aspect = props[0];
+        var subpage = props[1];
+        var href = urlfor + aspect + '/' + q  + '/' + subpage
+        $( '#aspect-chooser' ).append(
+        `<a role="button" class="btn btn-outline-dark btn-sm" href="${href}">  ${subpage}  </a> ` );
+    });
+    } );
+    
+}
+
+function wembedder(q) {
+    var wembedderUrl = `https://wembedder.toolforge.org/api/most-similar/${q}`;
+    $.ajax({
+        url: wembedderUrl,
+        success: function (data) {
+            var html = `<hr><span data-toogle="tooltip" title="Related items from Wembedder knowledge graph embedding.">Related:</span> `; 
+            $( '#wembedder' ).append(html);
+
+            // Make list with results
+            data.most_similar.forEach(function(entry, idx, array) {
+                var listed_q = entry.item;
+                var language = 'en';
+
+                if (idx !== 0) {
+                    $( '#wembedder' ).append( ' &middot; ');
+                }
+
+                var html = '<a href="../' + listed_q + '"><span id="wembedder-result-' + listed_q + '">' + listed_q + '</span></a> '
+                $( '#wembedder' ).append( html );
+
+                // Convert Q identifier to labels
+                $.getJSON("https://www.wikidata.org/w/api.php?callback=?", {
+                    action: "wbgetentities",
+                    ids: listed_q,
+                    language: language,
+                    uselang: language,
+                    format: "json",
+                    strictlanguage: true,
+                }, function (data) {
+                    if (listed_q in data.entities) {
+                        label = entityToLabel(data.entities[listed_q],
+                                            language=language);
+                        $('#wembedder-result-' + listed_q).empty();
+                        $('#wembedder-result-' + listed_q).text(label);
+                    }
+                });
+            });
+            $( '#wembedder' ).append( '<hr>' );
+        }
+    });
+    
+}
+
+function searchTerm(placeholder) {
+
+	let search_text = ""
+	$('#searchterm').select2({
+		allowClear: true,
+		placeholder: placeholder,
+		ajax: {
+			url: "https://www.wikidata.org/w/api.php?callback=?",
+			dataType: 'jsonp',
+			data: function (params) {
+				search_text = params.term
+				var query = {
+					search: params.term,
+					action: "wbsearchentities",
+					language: "en",
+					uselang: "en",
+					format: "json",
+					strictlanguage: true
+				}
+				return query
+			},
+			processResults: function (data) {
+				var results = [{id: -1, text: "Advanced search"}];
+				$.each(data.search, function (index, item) {
+					results.push({
+						id: "{{ url_for('app.index') }}" + item.title,
+						text: item.label + " - " + item.description
+					});
+				});
+
+				return {
+					"results": results
+				};
+			}
+		}
+	});
+
+	$("#searchterm").on("select2:select", function (e) {
+		const search_id = e.params.data.id
+		if (search_id === -1) {
+			if(search_text === undefined) search_text = ""
+			window.location.href = "/search?q=" + search_text;
+		} else {
+			window.location.href = e.params.data.id;
+		}
+	});	
+	
+	
+}
