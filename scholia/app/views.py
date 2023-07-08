d00496715c4159ff4f47a6c2b0349bdf13a4e0ba
Carlin MacKenzie <carlin.mackenzie@gmail.com>
2023-07-08 14:31:26 +0100

    fix spacing

--- scholia/app/views.py
+++ scholia/app/views.py
@@ -2471,7 +2471,6 @@ def show_about():
     return render_template('about.html')
 
 
-
 @main.route('/wikiproject/')
 def show_wikiproject_index():
     """Return rendered HTML index page for wikiproject.
@@ -2483,7 +2482,6 @@ def show_wikiproject_index():
 
     """
     return render_template('wikiproject-index.html')
-
 
 
 @main.route('/wikiproject/' + q_pattern)
