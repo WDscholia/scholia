1e7161d43bac05385a1e5b9fe8591ebe74311901
faresh9 <far0213025@ju.edu.jo>
2024-02-04 21:35:46 +0300

    Refactor ojs.py: Remove headers from requests.get and add new xpath for 'summary_title' class

--- scholia/scrape/ojs.py
+++ scholia/scrape/ojs.py
@@ -82,7 +82,7 @@ def issue_url_to_paper_urls(url):
     URLs.
 
     """
-    response = requests.get(url, headers=HEADERS)
+    response = requests.get(url)
     tree = etree.HTML(response.content)
     urls = tree.xpath("//div[@class='title']/a/@href")
     if len(urls) == 0:
@@ -93,10 +93,13 @@ def issue_url_to_paper_urls(url):
         # For instance, for https://tidsskrift.dk/bras/issue/view/9150
         urls = tree.xpath("//h3[@class='media-heading']/a/@href")
     if len(urls) == 0:
-        # For instance, for
-        # https://www.journals.vu.lt/scandinavistica/issue/view/1153
+        # For instance, for https://www.journals.vu.lt/scandinavistica/issue/view/1153
         urls = tree.xpath("//div[@class='article-summary-title']/a/@href")
+    if len(urls) == 0:
+        # Targeting specific class 'summary_title'
+        urls = tree.xpath("//a[@class='summary_title']/@href")
     return urls
+
 
 
 def issue_url_to_quickstatements(url, iso639=None):
