658b8dac05ff0cece2157f3e12d0d8cc26eca668
Hannah Bast <bast@cs.uni-freiburg.de>
2025-09-20 11:20:26 +0200

    Serve via scholia.wiki domain

--- runserver.py
+++ runserver.py
@@ -1,5 +1,9 @@
 from scholia.app import create_app
 import sys
+import logging
+
+# Enable debug logging
+logging.basicConfig(level=logging.DEBUG)
 
 app = create_app(
     text_to_topic_q_text_enabled=False,
@@ -8,6 +12,6 @@ app.config['APPLICATION_ROOT'] = '/'
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
-        app.run(debug=True, host='tajo', port=sys.argv[1])
+        app.run(debug=True, host='0.0.0.0', port=sys.argv[1])
     else:
-        app.run(debug=True, host='tajo', port=8100)
+        app.run(debug=True, host='0.0.0.0', port=8100)
